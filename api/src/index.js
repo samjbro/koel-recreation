668ba64a170a84745c935c4c586c90d21f50c6f8
Sam Broughton <sjbroughton1@gmail.com>
2019-03-02 21:51:14 +0000

    api server listens on process.env.port

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ import '@babel/polyfill'
 
 import server from './server'
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
   console.log(`API listening on port ${PORT}`)
