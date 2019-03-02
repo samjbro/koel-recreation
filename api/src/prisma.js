6b0a16ffc31f5219a9a9524086e9159ab2e9824b
Sam Broughton <sjbroughton1@gmail.com>
2019-03-02 20:20:43 +0000

    adds prisma files

---
+++ api/src/prisma.js
@@ -0,0 +1,7 @@
+// import { Prisma } from 'prisma-binding'
+
+// const { PRISMA_HOST, PRISMA_PORT, PRISMA_SERVICE, PRISMA_STAGE, PRISMA_SECRET } = process.env
+
+// const endpoint = `http://${PRISMA_HOST}:${PRISMA_PORT}/${PRISMA_SERVICE}/${PRISMA_STAGE}`
+
+// const prisma = new Prisma({})
