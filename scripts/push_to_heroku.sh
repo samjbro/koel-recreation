1101d398bcead8d0c9b90a924a73da213a11068f
Sam Broughton <sjbroughton1@gmail.com>
2019-03-02 19:39:19 +0000

    fixes nginx port and deploys nginx last

--- scripts/push_to_heroku.sh
+++ scripts/push_to_heroku.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "$HEROKU_PASSWORD" | docker login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com
-docker tag samjbro/koel-recreation-nginx registry.heroku.com/koel-recreation/web
 docker tag samjbro/koel-recreation-client registry.heroku.com/koel-recreation-client/web
 docker tag samjbro/koel-recreation-api registry.heroku.com/koel-recreation-api/web
-docker push registry.heroku.com/koel-recreation/web
-heroku container:release web --app koel-recreation
+docker tag samjbro/koel-recreation-nginx registry.heroku.com/koel-recreation/web
 docker push registry.heroku.com/koel-recreation-client/web
 heroku container:release web --app koel-recreation-client
 docker push registry.heroku.com/koel-recreation-api/web
 heroku container:release web --app koel-recreation-api
+docker push registry.heroku.com/koel-recreation/web
+heroku container:release web --app koel-recreation
