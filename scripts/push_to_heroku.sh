2cb089ad07e67efd20366879b8200d7496a4ff24
Sam Broughton <sjbroughton1@gmail.com>
2019-03-02 19:08:01 +0000

    trying out heroku deploy again

--- scripts/push_to_heroku.sh
+++ scripts/push_to_heroku.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "$HEROKU_SECRET_KEY" | docker login --username="$DOCKER_ID" --password-stdin registry.heroku.com
-# docker tag samjbro/koel-recreation-nginx registry.heroku.com/koel-recreation/web
-docker tag samjbro/koel-recreation-client registry.heroku.com/koel-recreation/web
-# docker tag samjbro/koel-recreation-api registry.heroku.com/koel-recreation/api
+docker tag samjbro/koel-recreation-nginx registry.heroku.com/koel-recreation/web
+docker tag samjbro/koel-recreation-client registry.heroku.com/koel-recreation-client/web
+docker tag samjbro/koel-recreation-api registry.heroku.com/koel-recreation-api/web
 docker push registry.heroku.com/koel-recreation/web
-# docker push registry.heroku.com/koel-recreation/client
-# docker push registry.heroku.com/koel-recreation/api
+heroku container:release web --app koel-recreation
+docker push registry.heroku.com/koel-recreation-client/web
+heroku container:release web --app koel-recreation-client
+docker push registry.heroku.com/koel-recreation-api/web
+heroku container:release web --app koel-recreation-api
