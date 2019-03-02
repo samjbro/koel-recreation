41677a6db4138a92f0757b55c8eb860b9e3f4a72
Sam Broughton <sjbroughton1@gmail.com>
2019-03-02 18:08:49 +0000

    attempts push to heroku

--- scripts/push_to_heroku.sh
+++ scripts/push_to_heroku.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker login --username="$DOCKER_ID" --password="$HEROKU_SECRET_KEY" registry.heroku.com
+echo "$HEROKU_SECRET_KEY" | docker login --username="$DOCKER_ID" --password-stdin registry.heroku.com
 docker tag samjbro/koel-recreation-nginx registry.heroku.com/koel-recreation/web
 docker tag samjbro/koel-recreation-client registry.heroku.com/koel-recreation/client
 docker tag samjbro/koel-recreation-api registry.heroku.com/koel-recreation/api
+docker push registry.heroku.com/koel-recreation/web
+docker push registry.heroku.com/koel-recreation/client
+docker push registry.heroku.com/koel-recreation/api
