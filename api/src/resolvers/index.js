ad3d7ceaf0de0c3bd60fa18198a7015a827cfe6c
Sam Broughton <sjbroughton1@gmail.com>
2019-03-03 19:53:23 +0000

    adds in proxy headers but leaves out proxy http version to see if the heroku issue still occurs

--- api/src/resolvers/index.js
+++ api/src/resolvers/index.js
@@ -1,9 +1,11 @@
 import Query from './query'
 import Mutation from './mutation'
+import Subscription from './subscription'
 
 const resolvers = {
   Query,
-  Mutation
+  Mutation,
+  Subscription
 }
 
 export { resolvers as default }
