095d8de75748a2a7aca2f25d55b469f7c5b4052f
Sam Broughton <sjbroughton1@gmail.com>
2019-03-03 17:57:29 +0000

    testing regular api requests

--- api/src/server.js
+++ api/src/server.js
@@ -9,12 +9,18 @@ import resolvers from './resolvers'
 const app = express()
 // app.use(cors())
 
+
 const apolloServer = new ApolloServer({
   typeDefs: importSchema('./src/schema.graphql'),
   resolvers,
   context: request => ({ request, prisma })
 })
 
-apolloServer.applyMiddleware({ app, path: '/graphql' })
+// apolloServer.applyMiddleware({ app, path: '/graphql' })
+
+
+app.get('/graphql/test', (req, res) => {
+  res.send(200, 'API RESPONDING')
+})
 
 export { app as default }
